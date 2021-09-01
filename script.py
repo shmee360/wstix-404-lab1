a3b5793a8b64d6bb91a8b2dbd2d0a0a79ccc55d4
Warren <wstix@ualberta.ca>
2021-09-01 15:50:59 -0600

    final commit probably

--- script.py
+++ script.py
@@ -2,9 +2,12 @@
 
 import requests
 
-print('Module Version:', requests.__version__, end='\n\n')
+# print('Module Version:', requests.__version__, end='\n\n')
 
-r = requests.get('http://www.google.com/')
+# r = requests.get('http://www.google.com/')
+r = requests.get('https://raw.githubusercontent.com/shmee360/wstix-404-lab1/master/script.py')
 
-print('Status Code:', r.status_code, end='\n\n')
-print('Full Text:', r.text, sep='\n')
+# print('Status Code:', r.status_code, end='\n\n')
+# print('Full Text:', r.text, sep='\n')
+
+print(r.text)
